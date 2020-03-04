3527617a704f7dd9b671a0411a36c19277536e5b
ayushgoswami <47845740+ayushgoswami@users.noreply.github.com>
2020-03-04 09:28:52 -0800

    Update DummyProject.py

--- DummyProject/DummyProject.py
+++ DummyProject/DummyProject.py
@@ -1,6 +1,17 @@
+import os
+
 class DummyProject:
   def __init__(self):
     pass
   
   def testme(self):
     return "testing me"
+  
+  def checklog(self, filename):
+    dstdir = os.path.join(os.getcwd(), "Logs")
+    if(not os.path.isdir(dstdir)):
+      os.mkdir("Logs")
+    logfile = os.path.join(dstdir, filename)
+    lfile = open(logfile, 'w')
+    lfile.write("testing log creation")
+    lfile.close()
